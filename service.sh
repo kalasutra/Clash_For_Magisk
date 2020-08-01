59872a30db2566b6370a135b6112ca1948cbbf34
OoHHHHHHH <68913786+OoHHHHHHH@users.noreply.github.com>
2020-08-01 12:47:04 +0800

    add start with Android

--- service.sh
+++ service.sh
@@ -3,6 +3,11 @@
 until [ $(getprop sys.boot_completed) -eq 1 ]; do
   sleep 5
 done
+# start with Android
+while [[ ! -d "/sdcard/Documents" ]];do
+    sleep 1
+done
+clash_control enable
 # default disable ipv6 accept_ra
 echo 0 > /proc/sys/net/ipv6/conf/all/accept_ra
 echo 0 > /proc/sys/net/ipv6/conf/wlan0/accept_ra
