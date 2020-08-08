91ea820b28e278685d53fe058c576e7b6d2e9e6a
kalasutra <naraka.manjusaka+github@gmail.com>
2020-08-08 11:48:50 +0800

    Change to online mode

--- customize.sh
+++ customize.sh
@@ -1,43 +1,97 @@
-#!/system/bin/sh
-
 # override the official Magisk module installer
 SKIPUNZIP=1
 
+download_command=""
+download_version=""
 sdcard_rw_id="1015"
+command_judgment="true"
 clash_data_dir="/sdcard/Documents/clash"
-pid_file="${clash_data_dir}/clash.pid"
-download_premium_link="https://github.com/Dreamacro/clash/releases/download/premium"
-download_command="wget --no-check-certificate -O ${MODPATH}/system/bin/clash.gz"
+download_location="${MODPATH}/system/bin/clash.gz"
+download_link="https://tmpclashpremiumbindary.cf"
 
-mkdir -p $MODPATH/system/bin
-mkdir -p ${clash_data_dir}
 
-download_premium() {
+if [ $BOOTMODE ! = true ] ; then
+    abort "! Please install in Magisk Manager"
+fi
+
+if $(curl -V > /dev/null 2>&1) ; then
+    download_command="curl -k -L -o ${download_location}"
+elif $(wget --help > /dev/null 2>&1) ; then
+    command_judgment="flase"
+    download_command="wget --no-check-certificate -O ${download_location}"
+else
+    abort "! Please install the busybox module and try again."
+fi
+
+download_canary_version() {
     case "${ARCH}" in
         arm)
-            $download_command ${download_premium_link}/clash-linux-armv7-2020.06.27.gz
+            if [ "${command_judgment}" == "true" ] ; then
+                download_version=$(curl -k -s ${download_link} | grep -o clash-linux-armv7-*.*.*.*.gz | awk -F '>' '{print $2}')
+                ${download_command} ${download_link}/${download_version}
+            else
+                touch ${TMPDIR}/version
+                wget --no-check-certificate -O ${TMPDIR}/version ${download_link}
+                download_version=$(cat ${TMPDIR}/version | grep -o clash-linux-armv7-*.*.*.*.gz | awk -F '>' '{print $2}')
+                ${download_command} ${download_link}/${download_version}
+            fi
             ;;
         arm64)
-            $download_command ${download_premium_link}/clash-linux-armv8-2020.06.27.gz
+            if [ "${command_judgment}" == "true" ] ; then
+                download_version=$(curl -k -s ${download_link} | grep -o clash-linux-armv8-*.*.*.*.gz | awk -F '>' '{print $2}')
+                ${download_command} ${download_link}/${download_version}
+            else
+                touch ${TMPDIR}/version
+                wget --no-check-certificate -O ${TMPDIR}/version ${download_link}
+                download_version=$(cat ${TMPDIR}/version | grep -o clash-linux-armv8-*.*.*.*.gz | awk -F '>' '{print $2}')
+                ${download_command} ${download_link}/${download_version}
+            fi
             ;;
         x86)
-            $download_command ${download_premium_link}/clash-linux-386-2020.06.27.gz
+            if [ "${command_judgment}" == "true" ] ; then
+                download_version=$(curl -k -s ${download_link} | grep -o clash-linux-386-*.*.*.*.gz | awk -F '>' '{print $2}')
+                ${download_command} ${download_link}/${download_version}
+            else
+                touch ${TMPDIR}/version
+                wget --no-check-certificate -O ${TMPDIR}/version ${download_link}
+                download_version=$(cat ${TMPDIR}/version | grep -o clash-linux-386-*.*.*.*.gz | awk -F '>' '{print $2}')
+                ${download_command} ${download_link}/${download_version}
+            fi
             ;;
         x64)
-            $download_command ${download_premium_link}/clash-linux-amd64-2020.06.27.gz
+            if [ "${command_judgment}" == "true" ] ; then
+                download_version=$(curl -k -s ${download_link} | grep -o clash-linux-amd64-*.*.*.*.gz | awk -F '>' '{print $2}')
+                ${download_command} ${download_link}/${download_version}
+            else
+                touch ${TMPDIR}/version
+                wget --no-check-certificate -O ${TMPDIR}/version ${download_link}
+                download_version=$(cat ${TMPDIR}/version | grep -o clash-linux-amd64-*.*.*.*.gz | awk -F '>' '{print $2}')
+                ${download_command} ${download_link}/${download_version}
+            fi
             ;;
     esac
 }
 
-download_premium
-gzip -d ${MODPATH}/system/bin/clash.gz
+mkdir -p $MODPATH/system/bin
+mkdir -p ${clash_data_dir}
+
+download_canary_version
+gzip -d ${download_location}
 unzip -j -o "${ZIPFILE}" 'clash_control.sh' -d $MODPATH/system/bin >&2
 unzip -j -o "${ZIPFILE}" 'clash_service.sh' -d $MODPATH >&2
 unzip -j -o "${ZIPFILE}" 'clash_tproxy.sh' -d $MODPATH >&2
 unzip -j -o "${ZIPFILE}" 'service.sh' -d $MODPATH >&2
 unzip -j -o "${ZIPFILE}" 'uninstall.sh' -d $MODPATH >&2
-unzip -j -o "${ZIPFILE}" 'module.prop' -d $MODPATH >&2
 mv $MODPATH/system/bin/clash_control.sh $MODPATH/system/bin/clash_control
+
+rm -rf $MODPATH/module.prop
+touch $MODPATH/module.prop
+echo "id=clash_premium" > $MODPATH/module.prop
+echo "name=Clash Premium For Magisk" >> $MODPATH/module.prop
+echo "version=${download_version}" >> $MODPATH/module.prop
+echo "versionCode=$(date +%Y%m%d)" >> $MODPATH/module.prop
+echo "author=shell scripts by kalasutra. clash premium by Dreamacro" >> $MODPATH/module.prop
+echo "description=clash premium with service scripts for Android.Only supports tun mode transparent proxy.Default disable ipv6." >> $MODPATH/module.prop
 
 set_perm_recursive $MODPATH 0 0 0755 0644
 set_perm  $MODPATH/service.sh    0  0  0755
